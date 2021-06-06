6ee480fc5ad941224be35f98069d3c0fa3284195
김명수 <msbmkim@gmail.com>
2021-06-06 18:27:41 +0900

    Update Main.java

--- sources/Main.java
+++ sources/Main.java
@@ -7,37 +7,115 @@ public class Main {
 	public static Scanner sc = new Scanner(System.in);
 	public static StringTokenizer st;
 	
+	public static int N;
+	public static int M;
+	public static int[][] arr;
+	
 	public static void main(String[] args) throws Exception {
-		br = new BufferedReader(new FileReader(new File("/Users/gimmyeongsu/Desktop/Intellij/BOJ/src/input")));
-		
-		int N,K;
-		st = new StringTokenizer(br.readLine());
+		st= new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		
-		int idx = 1;
-		while(st.hasMoreTokens()){
-			int n = Integer.parseInt(st.nextToken());
-//			arr[idx] = arr[idx-1] + n;
-			arr[idx] = n;
-			++idx;
-		}
-		
-		for(int tc=0; tc<K; ++tc){
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for(int i=0; i<N; ++i){
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			double l = e - s + 1;
-			double sum = 0;
-			for(int i=s; i<=e; ++i) sum += arr[i];
-			System.out.printf("%.02f\n", Math.round(100d * sum / l) / 100d);
+			for(int j=0; j<M; ++j){
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
+		
+		int result = 0;
+		int cnt = 0;
+		while(true){
+			if(!isCheck()){
+				break;
+			}
+			++result;
+			cnt = getCheese();
+			
+			visit = new boolean[N][M];
+			dfs(0,0);
+			
+//			print();
+			
+			for(int i=0; i<N; ++i){
+				for(int j=0; j<M; ++j){
+					if(arr[i][j] == 1) {
+						boolean check = false;
+						for (int k = 0; k < 4; ++k) {
+							int nr = i + rd[k];
+							int nc = j + cd[k];
+							try {
+								if (arr[nr][nc] == 2) {
+									check = true;
+									break;
+								}
+							} catch (Exception e) {
+							}
+						}
+						if (check) {
+							arr[i][j] = 0;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(result);
+		System.out.println(cnt);
+		
 		
 		sc.close();
 		bw.close();
 		br.close();
+	}
+	
+	public static int[] rd = {1, -1, 0,  0};
+	public static int[] cd = {0,  0, 1, -1};
+	public static boolean[][] visit;
+	public static void dfs(int r, int c){
+		visit[r][c] = true;
+		if(arr[r][c] == 1){
+			return;
+		}
+		arr[r][c] = 2;
+		for(int i=0; i<4; ++i){
+			try{
+				int nr = r + rd[i];
+				int nc = c + cd[i];
+				if(!visit[nr][nc]){
+					dfs(nr,nc);
+				}
+			}catch(Exception e){}
+		}
+	}
+	
+	public static boolean isCheck(){
+		for(int i=0; i<N; ++i){
+			for(int j=0; j<M; ++j){
+				if(arr[i][j] == 1){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static int getCheese(){
+		int result = 0;
+		for(int i=0; i<N; ++i){
+			for(int j=0; j<M; ++j){
+				if(arr[i][j] == 1){
+					++result;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public static void print(){
+		System.out.println("--------------------------------------------------------");
+		for(int i=0; i<N; ++i){
+			System.out.println(Arrays.toString(arr[i]).replaceAll(",", "").replaceAll("\\[", "").replaceAll("]", ""));
+		}
+		System.out.println("--------------------------------------------------------");
 	}
 }
