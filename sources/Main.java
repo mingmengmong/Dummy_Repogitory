ad4c9a653ce351b9a4bbc04a9843993cf53a8165
김명수 <msbmkim@gmail.com>
2021-06-07 17:26:45 +0900

    Update Main.java

--- sources/Main.java
+++ sources/Main.java
@@ -7,115 +7,64 @@ public class Main {
 	public static Scanner sc = new Scanner(System.in);
 	public static StringTokenizer st;
 	
-	public static int N;
-	public static int M;
-	public static int[][] arr;
+	
+	// input field
+	static int N, A, T;
+	static int[][] arr;
+	
+	// result
+	static int result = Integer.MAX_VALUE;
+	
+	
+	static void dfs(int idx, int[] sumArr, int sumCost){
+		if(idx == N) return;
+		if(isCheck(sumArr)){
+			result = Math.min(sumCost, result);
+			return;
+		}
+		
+		for(int i=idx; i<N; ++i){
+			for(int j=0; j<A; ++j)
+				sumArr[j] += arr[i][j+1];
+			dfs(idx+1, sumArr, sumCost + arr[i][0]);
+			for(int j=0; j<A; ++j)
+				sumArr[j] -= arr[i][j+1];
+		}
+		
+		dfs(idx+1, sumArr, sumCost);
+	}
+	
+	static boolean isCheck(int[] sumArr){
+		for(int i=0; i<A; ++i){
+			if(sumArr[i] < T) return false;
+		}
+		return true;
+	}
 	
 	public static void main(String[] args) throws Exception {
-		st= new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
+		A = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N][A+1];
 		for(int i=0; i<N; ++i){
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; ++j){
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			for(int j=1; j<=A; ++j){
+				arr[i][j] = Integer.parseInt(st.nextToken());
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int result = 0;
-		int cnt = 0;
-		while(true){
-			if(!isCheck()){
-				break;
-			}
-			++result;
-			cnt = getCheese();
-			
-			visit = new boolean[N][M];
-			dfs(0,0);
-			
-//			print();
-			
-			for(int i=0; i<N; ++i){
-				for(int j=0; j<M; ++j){
-					if(arr[i][j] == 1) {
-						boolean check = false;
-						for (int k = 0; k < 4; ++k) {
-							int nr = i + rd[k];
-							int nc = j + cd[k];
-							try {
-								if (arr[nr][nc] == 2) {
-									check = true;
-									break;
-								}
-							} catch (Exception e) {
-							}
-						}
-						if (check) {
-							arr[i][j] = 0;
-						}
-					}
-				}
-			}
-		}
-		System.out.println(result);
-		System.out.println(cnt);
+		Arrays.sort(arr, (ar1, ar2) -> Integer.compare(ar1[0], ar2[0]));
 		
+		dfs(0, new int[A], 0);
+		
+		System.out.println(result);
 		
 		sc.close();
 		bw.close();
 		br.close();
-	}
-	
-	public static int[] rd = {1, -1, 0,  0};
-	public static int[] cd = {0,  0, 1, -1};
-	public static boolean[][] visit;
-	public static void dfs(int r, int c){
-		visit[r][c] = true;
-		if(arr[r][c] == 1){
-			return;
-		}
-		arr[r][c] = 2;
-		for(int i=0; i<4; ++i){
-			try{
-				int nr = r + rd[i];
-				int nc = c + cd[i];
-				if(!visit[nr][nc]){
-					dfs(nr,nc);
-				}
-			}catch(Exception e){}
-		}
-	}
-	
-	public static boolean isCheck(){
-		for(int i=0; i<N; ++i){
-			for(int j=0; j<M; ++j){
-				if(arr[i][j] == 1){
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	public static int getCheese(){
-		int result = 0;
-		for(int i=0; i<N; ++i){
-			for(int j=0; j<M; ++j){
-				if(arr[i][j] == 1){
-					++result;
-				}
-			}
-		}
-		return result;
-	}
-	
-	public static void print(){
-		System.out.println("--------------------------------------------------------");
-		for(int i=0; i<N; ++i){
-			System.out.println(Arrays.toString(arr[i]).replaceAll(",", "").replaceAll("\\[", "").replaceAll("]", ""));
-		}
-		System.out.println("--------------------------------------------------------");
 	}
 }
